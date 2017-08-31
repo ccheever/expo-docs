9dfe21f766feb4254b1a621fc042a07523948bc4
Charlie Cheever <ccheever@gmail.com>
2017-08-30 18:25:02 -0700

    Complete table of contents
    Also adding files that I forgot git add last time

---
+++ pages/docs/introduction/community.js
@@ -0,0 +1,28 @@
+import markdown from 'markdown-in-js'
+import withDoc, { components } from '../../../lib/with-doc'
+
+import { ccheever } from '../../../lib/data/team'
+// import { InternalLink, ExternalLink } from "../../../components/text/link";
+// import { P } from "../../../components/text/paragraph";
+// import Image from '../../../components/image'
+// import { InlineCode } from "../../../components/text/code";
+// import {
+//   TerminalInput,
+//   TerminalOutput
+// } from "../../../components/text/terminal";
+
+// prettier-ignore
+export default withDoc({
+  title: 'Community',
+  date: '30 Aug 2017',
+  authors: [ccheever],
+})(markdown(components)`
+
+Want to chat about Expo? The best way to get in touch with our team and other people developing with Expo is to join our Slack chat. We always like to hear about projects and components that people are building on Expo and there's usually someone available to answer questions.
+
+- [Join our forums](http://forums.expo.io/)
+- [Follow us on Twitter @expo_io](https://twitter.com/expo_io)
+- [Become a contributor on Github](https://github.com/expo)
+- [Request an invitation](https://slack.expo.io/) to our [Slack](https://expo-developers.slack.com/).
+
+`)
