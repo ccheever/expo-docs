5a3dfbc948baea9045954611a67b17957857b05d
sameesiddiqui <sameezsiddiqui@gmail.com>
2017-09-02 19:22:54 -0700

    svg

---
+++ pages/docs/sdk/svg.js
@@ -0,0 +1,32 @@
+import markdown from 'markdown-in-js'
+import withDoc, { components } from '../../../lib/with-doc'
+
+import { ccheever } from '../../../lib/data/team'
+// import { InternalLink, ExternalLink } from "../../../components/text/link";
+// import { P } from "../../../components/text/paragraph";
+// import Image from '../../../components/image'
+// import { Code } from '../../../components/text/code'
+import SnackEmbed from '../../../components/snack-embed'
+// import {
+//   TerminalInput,
+//   TerminalOutput
+// } from "../../../components/text/terminal";
+
+// prettier-ignore
+export default withDoc({
+  title: 'Svg',
+  date: '30 Aug 2017',
+  authors: [ccheever],
+})(markdown(components)`
+
+### Expo.Svg()
+
+A set of drawing primitives such as \`Circle\`, \`Rect\`, \`Path\`,
+\`ClipPath\`, and \`Polygon\`. It supports most SVG elements and properties.
+The implementation is provided by
+[react-native-svg](https://github.com/react-native-community/react-native-svg),
+and documentation is provided in that repository.
+
+${<SnackEmbed snackId='HJ1m5ICJb' />}
+
+`)
--- pages/docs/sdk/util.js
+++ pages/docs/sdk/util.js
@@ -18,7 +18,8 @@ export default withDoc({
   authors: [ccheever],
 })(markdown(components)`
 
-Helpful utility functions that don't fit anywhere else, including some localization / i18n methods.
+Helpful utility functions that don't fit anywhere else, including some
+localization / i18n methods.
 
 ### Expo.Util.getCurrentDeviceCountryAsync()
 
@@ -34,21 +35,26 @@ Returns the current device time zone name.
 
 ### Expo.Util.reload()
 
-Reloads the current experience. This will fetch and load the newest available JS supported by the device's Expo environment. This is useful for triggering an update of your experience if you have published a new version.
+Reloads the current experience. This will fetch and load the newest available
+JS supported by the device's Expo environment. This is useful for triggering an
+update of your experience if you have published a new version.
 
 ## Subscribing to App Updates
 
 ### Expo.Util.addNewVersionListenerExperimental(listener)
 
-_Android only_. Invokes a callback when a new version of your app is successfully downloaded in the background.
+_Android only_. Invokes a callback when a new version of your app is
+successfully downloaded in the background.
 
 #### Arguments
 
--   **listener (_function_)** -- A callback that is invoked when a new version of your app is successfully downloaded in the background.
+-   **listener (_function_)** -- A callback that is invoked when a new version
+of your app is successfully downloaded in the background.
 
 #### Returns
 
-An [EventSubscription](#eventsubscription) object that you can call remove() on when you would like to unsubscribe from the listener.
+An [EventSubscription](#eventsubscription) object that you can call remove()
+on when you would like to unsubscribe from the listener.
 
 ### Related types
 
