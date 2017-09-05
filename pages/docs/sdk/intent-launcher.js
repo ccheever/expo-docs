ac7af1d1ab9fb98910eae5dd543bd3dc19d54f88
sameesiddiqui <sameezsiddiqui@gmail.com>
2017-09-05 01:39:20 -0700

    imagepicker, intent-launcher, keepawake, lineargradient, location

---
+++ pages/docs/sdk/intent-launcher.js
@@ -0,0 +1,40 @@
+import markdown from 'markdown-in-js'
+import withDoc, { components } from '../../../lib/with-doc'
+
+import { ccheever } from '../../../lib/data/team'
+// import { InternalLink, ExternalLink } from "../../../components/text/link";
+// import { P } from "../../../components/text/paragraph";
+// import Image from '../../../components/image'
+import { Code } from '../../../components/text/code'
+// import {
+//   TerminalInput,
+//   TerminalOutput
+// } from "../../../components/text/terminal";
+
+// prettier-ignore
+export default withDoc({
+  title: 'Intent Launcher (Android)',
+  date: '4 Sep 2017',
+  authors: [ccheever],
+})(markdown(components)`
+Provides a way to launch android intents. e.g. - opening a specific settings screen.
+
+## Usage
+
+### Expo.IntentLauncherAndroid.startActivityAsync(activity, data)
+
+Starts the specified activity. The optional \`data\` parameter can be specified to pass additional data object to the activity. The method will return a promise which resolves when the user returns to the app.
+
+There are a few pre-defined constants you can use for the \`activity\` parameter. You can find them at [expo/expo-sdk/src/IntentLauncherAndroid.js](https://github.com/expo/expo-sdk/blob/master/src/IntentLauncherAndroid.js).
+
+#### Example
+
+${<Code>
+  {`import { IntentLauncherAndroid } from 'expo';
+
+  // Open location settings
+  IntentLauncherAndroid.startActivityAsync(
+    IntentLauncherAndroid.ACTION_LOCATION_SOURCE_SETTINGS
+  );`}</Code>}
+
+`)
---
+++ pages/docs/sdk/keepawake.js
@@ -0,0 +1,77 @@
+import markdown from 'markdown-in-js'
+import withDoc, { components } from '../../../lib/with-doc'
+
+import { ccheever } from '../../../lib/data/team'
+// import { InternalLink, ExternalLink } from "../../../components/text/link";
+// import { P } from "../../../components/text/paragraph";
+// import Image from '../../../components/image'
+import { Code } from '../../../components/text/code'
+// import {
+//   TerminalInput,
+//   TerminalOutput
+// } from "../../../components/text/terminal";
+
+// prettier-ignore
+export default withDoc({
+  title: 'KeepAwake',
+  date: '4 Sep 2017',
+  authors: [ccheever],
+})(markdown(components)`
+A React component that prevents the screen sleeping when rendered. It also exposes static methods to control the behavior imperatively.
+
+## Example: component
+
+${<Code>
+  {`import React from 'react';
+  import {
+    Text,
+    StyleSheet,
+    View,
+  } from 'react-native';
+  import { KeepAwake } from 'expo';
+
+  export default class KeepAwakeExample extends React.Component {
+    render() {
+      return (
+        <View>
+          <KeepAwake />
+          <Text>This screen will never sleep!</Text>
+        </View>
+      );
+    }
+  }`}
+  </Code>}
+
+### Example: static methods
+
+${<Code>
+{`import React from 'react';
+import {
+  Button,
+  StyleSheet,
+  View,
+} from 'react-native';
+import { KeepAwake } from 'expo';
+
+export default class KeepAwakeExample extends React.Component {
+
+  _activate = () => {
+    KeepAwake.activate();
+  }
+
+  _deactivate = () => {
+    KeepAwake.deactivate();
+  }
+
+  render() {
+    return (
+      <View>
+        <Button onPress={this._activate}>Activate</Button>
+        <Button onPress={this._deactivate}>Deactivate</Button>
+      </View>
+    );
+  }
+}
+`}</Code>}
+
+`)
