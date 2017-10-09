27387ebeb18f5d90e90385061e58db679580ecdd
Abi Raja <abimanyuraja@gmail.com>
2017-10-09 11:31:12 -0700

    Enable console.logs in eslint rules, and print URL that the server is serving at

--- pages/versions/v20.0.0/guides/assets.js
+++ pages/versions/v20.0.0/guides/assets.js
@@ -2,14 +2,7 @@ import markdown from 'markdown-in-js'
 import withDoc, { components } from '~/lib/with-doc'
 
 import { expoteam } from '~/data/team'
-// import { InternalLink, ExternalLink } from "~/components/text/link";
-// import { P } from "~/components/text/paragraph";
-// import Image from '~/components/image'
 import { InlineCode } from '~/components/text/code'
-// import {
-//   TerminalInput,
-//   TerminalOutput
-// } from "~/components/text/terminal";
 
 // prettier-ignore
 export default withDoc({
