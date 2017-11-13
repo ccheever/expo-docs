7ac1f49526c0e9e2db4f4076918e51a28743d70e
Abi Raja <abimanyuraja@gmail.com>
2017-11-13 10:33:35 -0500

    Support permalinks for lists and h4s

--- components/base/permalink.js
+++ components/base/permalink.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import PermalinkIcon from '~/components/icons/permalink-icon'
-import { FONT_FAMILY_MONO, FONT_FAMILY_SANS_BOLD } from '~/style/css-config'
 
 const slugs = require(`github-slugger`)()
 
-// Heading that supports permalink
-
-class Heading extends React.Component {
+class Permalink extends React.Component {
   render() {
     const { component, className, children, ...rest } = this.props
     return React.cloneElement(
@@ -32,7 +29,7 @@ const fn = props => {
   }
 
   return (
-    <Heading
+    <Permalink
       className={props.lean ? 'lean' : ''}
       component={component}
       data-components-heading
@@ -94,94 +91,8 @@ const fn = props => {
         }
         `}
       </style>
-    </Heading>
+    </Permalink>
   )
 }
 
 export default fn
-
-export const H1 = ({ children }) => (
-  <h1>
-    {children}
-    <style jsx>
-      {`
-      h1 {
-        font-size: 2.25rem;
-        line-height: 2.75rem;
-        margin-bottom: 1.5rem;
-      }
-    `}
-    </style>
-  </h1>
-)
-
-const B = ({ children }) => (
-  <span>
-    {children}
-    <style jsx>
-      {`
-      span {
-        font-family: ${FONT_FAMILY_SANS_BOLD};
-        font-weight: 400;
-      }
-    `}
-    </style>
-  </span>
-)
-
-H1.B = B
-
-export const H2 = ({ children }) => (
-  <h2>
-    {children}
-    <style jsx>
-      {`
-      h2 {
-        font-family: ${FONT_FAMILY_SANS_BOLD};
-        font-weight: 400;
-        line-height: 1.75rem;
-        font-size: 1.875rem;
-        margin-bottom: 1.5rem;
-      }
-    `}
-    </style>
-  </h2>
-)
-
-export const H3 = ({ children }) => (
-  <h3>
-    {children}
-    <style jsx>
-      {`
-      h3 {
-        font-size: 1.375rem;
-        line-height: 1.75rem;
-        margin-bottom: 1.5rem;
-      }
-    `}
-    </style>
-  </h3>
-)
-
-export const H4 = ({ children, isCommand }) => (
-  <h4 className={isCommand ? 'command' : ''}>
-    {children}
-    <style jsx>
-      {`
-      h4 {
-        font-family: ${FONT_FAMILY_SANS_BOLD};
-        font-weight: 400;
-        line-height: 1.625rem;
-        font-size: 1.1rem;
-        margin-bottom: 0.25rem;
-      }
-
-      .command {
-        font-family: ${FONT_FAMILY_MONO};
-        font-size: 0.9em;
-        color: #bd10e0;
-      }
-    `}
-    </style>
-  </h4>
-)
