eca1845bd9110bf713e3448e9daca3616acb7100
Abi Raja <abimanyuraja@gmail.com>
2017-11-13 14:17:02 -0500

    Fix footer link

--- pages/versions/v20.0.0/guides/index.js
+++ pages/versions/v20.0.0/guides/index.js
@@ -10,6 +10,6 @@ export default withDoc({
   authors: [expoteam],
 })(markdown(components)`
 
-These guides cover specific goals or features you might want to implement in your Expo project. For example, many people want to [load custom fonts](./using-custom-fonts) besides those which come with the device, or [send push notifications](./push-notifications) to their users. Read whichever guide suits your needs.
+These guides cover specific goals or features you might want to implement in your Expo project. For example, many people want to [load custom fonts](./custom-fonts) besides those which come with the device, or [send push notifications](./push-notifications) to their users. Read whichever guide suits your needs.
 
 `)
